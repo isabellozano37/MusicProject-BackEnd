1e76b85136ca9efd07ffba708e752bd751475bcf
isabellozano37 <isabeloficial37@gmail.com>
2023-09-25 10:32:23 +0200

    Agregar archivos de proyecto.

---
+++ Data/Migrations/20230925081301_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id_Usuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre_Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellidos_Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion_Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id_Usuario);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Entities/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Usuario { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Apellidos_Usuario { get; set; }
+        public string Direccion_Usuario { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
